5321d198bfade0c33272aa19793663fc72fe7691
Vi-lka <vitalya.permyakov155@gmail.com>
2023-12-10 13:47:15 +0700

    style: dvh and vh for hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between pt-20">
-      <section className="w-full text-center mt-6 h-[80dvh] relative overflow-hidden">
+      <section className="hero w-full text-center mt-6 relative overflow-hidden">
         <h1 className="mt-6 px-6 mx-auto font-NotoSerif font-semibold 3xl:text-6xl 2xl:text-5xl xl:text-4xl lg:text-3xl sm:text-2xl text-xl">{dataResult.value.title}</h1>
         <h2 className="mx-auto mt-6 max-w-[18rem] font-Raleway font-normal lg:text-base sm:text-sm text-xs">{dataResult.value.subTitle}</h2>
         <BooksSvg className='xl:w-full lg:w-[150%] w-[200%] absolute bottom-0 left-1/2 -translate-x-1/2' />
